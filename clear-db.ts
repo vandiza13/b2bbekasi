212b7aa645e51ecb24f25be83538a9c215c2c0ae
Mr. Robot <tb.vandiza13@gmail.com>
2026-08-30 20:29:37 +0700

    chore: remove debug scripts

--- clear-db.ts
+++
@@ -1,8 +0,0 @@
-﻿import { db } from './src/db/index.ts';
-import { sql } from 'drizzle-orm';
-async function run() {
-  await db.execute(sql`TRUNCATE incident_tickets, sqm_tickets, outstanding_tickets, q_index_tickets, kpi_snapshots CASCADE;`);
-  console.log('DB Cleared');
-  process.exit(0);
-}
-run();
